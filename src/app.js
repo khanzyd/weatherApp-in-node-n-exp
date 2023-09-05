fa7431fd3811cc7011dd6f5aeb940f090a10c5b6
khanzyd <khanzyd7@gmail.com>
2023-09-05 20:10:47 +0530

    written code for handling form and getting data through api as per form request

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
-// const ejs = require("ejs");
 const path = require("path");
+const bodyparser = require("body-parser");
 
 const port = 3000;
 const app = new express();
+
+// importing own module for api ID
+const ID = require("../appID");
 
 // path for public directory
 const static_Path = path.join(__dirname , "../public");
@@ -11,22 +14,48 @@ const static_Path = path.join(__dirname , "../public");
 app.use(express.static(static_Path));
 app.set("view engine","ejs");
 
-
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended : true}))
 
 app.get("/" , (req,res) => {
     res.render("index");
 })
 
 app.get("/forecast" , (req,res) => {
-    res.render("forecast");
+    res.render("forecast",
+    {
+        cityName : "Mumbai",
+        cityDesc : "Sunny",
+        cityTemp : 27,
+        cityMinTemp : 23.4,
+        cityMaxTemp : 32.8
+    });
 })
 
-app.get("/forecast", (req,res)=>{
-    console.log();
-    // let inputvalue = ;
-    // res.render("inputvalue");
-    // console.log(inputvalue)
-    // console.log("hi")
+app.post("/forecast", async (req,res)=>{
+    let city_Name = req.body.formLtn;
+
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city_Name}&units=metric&appid=`
+
+    let data = await fetch(url);
+    let obj = await data.json();
+
+    city_Name = `${obj.name}, ${obj.sys.country}` ;
+    let city_Desc = obj.weather[0].main ;
+    let city_Temp = Math.floor(obj.main.temp) ;
+    let city_Mintemp = obj.main.temp_min ;
+    let city_Maxtemp = obj.main.temp_max ;
+
+
+    res.render("forecast" ,
+    {
+        cityName : city_Name,
+        cityDesc : city_Desc,
+        cityTemp : city_Temp,
+        cityMinTemp : city_Mintemp,
+        cityMaxTemp : city_Maxtemp
+    })
+    
 })
 
 app.listen(port , () => {
